e7d1f299476cdcd54133f68ecdda80123774e9ef
mkearney <kearneymw@missouri.edu>
2019-09-24 21:01:30 -0500

    Update

--- R/xgb_mat.R
+++ R/xgb_mat.R
@@ -1,29 +1,43 @@
-
+#' xgb matrix
+#'
+#' Creates C-style xgb matrix
+#'
+#' @param x Input data
+#' @param ... Other data to cbind
+#' @param y Label vector
+#' @param split Optional number between 0-1 indicating the desired split between
+#'  train and test
+#' @export
 xgb_mat <- function(x, ..., y = NULL, split = NULL) {
   UseMethod("xgb_mat")
 }
+
+#' @export
 xgb_mat.data.frame <- function(x, ..., y = NULL, split = NULL) {
   x <- as.matrix.data.frame(x, rownames.force = FALSE)
   xgb_mat(x, ..., y = y, split = split)
 }
 
+#' @export
+xgb_mat.default <- function(x, ..., y = NULL, split = NULL) {
+  x <- as.matrix(x)
+  xgb_mat(x, ..., y = y, split = split)
+}
+
+#' @export
 xgb_mat.matrix <- function(x, ..., y = NULL, split = NULL) {
   x <- cbind(x, ...)
   if (is.null(split)) {
     if (is.null(y)) {
-      #return(xgboost::xgb.DMatrix(x))
-      return(x)
+      return(xgboost::xgb.DMatrix(x))
     }
-    #return(xgboost::xgb.DMatrix(x, label = y))
-    return(cbind(x, label = y))
+    return(xgboost::xgb.DMatrix(x, label = y))
   }
   train_rows <- sample(seq_len(nrow(x)), nrow(x) * split)
   if (is.null(y)) {
     return(list(
-      #train = xgboost::xgb.DMatrix(x[train_rows, ]),
-      train = x[train_rows, ],
-      #test = xgboost::xgb.DMatrix(x[-train_rows, ])
-      test = x[-train_rows, ],
+      train = xgboost::xgb.DMatrix(x[train_rows, , drop = FALSE]),
+      test = xgboost::xgb.DMatrix(x[-train_rows, , drop = FALSE])
     ))
   }
   list(
@@ -34,6 +48,33 @@ xgb_mat.matrix <- function(x, ..., y = NULL, split = NULL) {
   )
 }
 
+#' @export
+xgb_mat.dgCMatrix <- function(x, ..., y = NULL, split = NULL) {
+  x <- cbind(x, ...)
+  if (is.null(split)) {
+    if (is.null(y)) {
+      return(xgboost::xgb.DMatrix(x))
+    }
+    return(xgboost::xgb.DMatrix(x, label = y))
+  }
+  train_rows <- sample(seq_len(nrow(x)), nrow(x) * split)
+  if (is.null(y)) {
+    return(list(
+      train = xgboost::xgb.DMatrix(x[train_rows, , drop = FALSE]),
+      test = xgboost::xgb.DMatrix(x[-train_rows, , drop = FALSE])
+    ))
+  }
+  list(
+    train = xgboost::xgb.DMatrix(x[train_rows, , drop = FALSE],
+      label = y[train_rows]),
+    test = xgboost::xgb.DMatrix(x[-train_rows, , drop = FALSE],
+      label = y[-train_rows])
+  )
+}
+
+
+
+#' @export
 xgb_mat.dgCMatrix <- function(x, ..., y = NULL, split = NULL) {
   x <- cbind(x, ...)
   if (is.null(split)) {
